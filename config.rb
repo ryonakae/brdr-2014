cc99d921827868c42ee041d28a81b56de2c4798b
RYO NAKAE <ryo.n.1129@gmail.com>
2014-05-28 01:53:42 +0900

    Sassファイルとか追加
    ここで力尽きた

---
+++ config.rb
@@ -0,0 +1,23 @@
+# Define Directory
+http_path = "files"
+sass_dir = "sass"
+css_dir = "files"
+fonts_dir = "files/font"
+images_dir = "files/img"
+javascripts_dir = "files/js"
+
+# Define Output Style
+# Option: expanded(default) / nested / compact / compressed
+output_style = :compressed
+
+# Define Comment
+line_comments = false
+
+# Define File Path
+relative_assets = true
+
+# Define SCSS or SASS
+# preferred_syntax = :scss
+
+# Disable .sass-cache
+cache = false
