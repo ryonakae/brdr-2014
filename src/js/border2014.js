002a7a457d6416d8896c4ab03ba1639e40d37933
RYO NAKAE <ryo.n.1129@gmail.com>
2014-06-11 21:07:23 +0900

    Blogまで完成

--- src/js/border2014.js
+++ src/js/border2014.js
@@ -59,7 +59,7 @@ jQuery(function(){
 
 
   // common loader fade out
-  function common_loader_fadeOut() {
+  function commonLoaderFadeOut() {
     jQuery(".l-loader")
       .transition({ "opacity" : 0 }, 1000)
       .transition({ "visibility" : "hidden" }, 0);
@@ -67,7 +67,7 @@ jQuery(function(){
 
 
   // common loader fade in
-  function common_loader_fadeIn() {
+  function commonLoaderFadeIn() {
     jQuery(".l-loader")
       .transition({ "visibility" : "visible" }, 0)
       .transition({ "opacity" : 1 }, 1000);
@@ -75,7 +75,7 @@ jQuery(function(){
 
 
   // index top resize
-  function index_top_resize() {
+  function indexTopResize() {
     jQuery(".l-index-top").css({
       "width": winWidth,
       "height": winHeight
@@ -84,34 +84,75 @@ jQuery(function(){
   }
 
   // index top scroll down
-  function index_top_scroll_down() {
+  function indexTopScrollDown() {
     var worksOffset = jQuery(".l-index-works").offset();
     var worksOffsetY = worksOffset.top;
+
+    var topImage = jQuery(".index-top-image");
+    var scroll;
+    var topHeight;
 
     jQuery(".scroll.scroll-down").click(function(){
       jQuery("html,body").animate({ scrollTop : worksOffsetY - 60 }, 1500, "easeInOutQuart");
     });
+
+    jQuery(window).scroll(function(){
+      topHeight = jQuery(window).height();
+      scroll = jQuery(window).scrollTop();
+
+      if ( scroll < 0 ) {
+        topImage.css({
+          y : 0,
+          "opacity" : 1
+        });
+      }
+      else if ( 0 <= scroll && scroll <= topHeight ) {
+        topImage.css({
+          y : scroll * -0.5,
+          "opacity" : 1 - (scroll / topHeight * 2)
+        });
+      }
+      else {
+        topImage.css({
+          y : 0,
+          "opacity" : 0
+        });
+      }
+    });
   }
 
   // index article resize
-  function index_article_resize() {
-    jQuery(".article img").imgResize();
+  function indexArticleImgResize() {
+    jQuery(".article").each(function(){
+      jQuery(this).find("img").imgResize()
+    });
+  }
+
+  // index blog article resize
+  function indexBlogArticleResize() {
+    var article = jQuery(".l-index-blog .article");
+    var articleWidth = article.width();
+
+    article.css({ "height" : articleWidth });
   }
 
 
   // load
   jQuery(window).on("load", function(){
-    common_loader_fadeOut();
+    commonLoaderFadeOut();
 
     jQuery(".logo").delay(1200).queue(function() {
       jQuery(this).addClass("is-shown").dequeue();
     });
 
-    index_top_resize();
+    indexTopResize();
 
-    index_top_scroll_down();
+    indexTopScrollDown();
 
-    index_article_resize();
+    indexBlogArticleResize();
+
+    indexArticleImgResize();
+
     jQuery(".article").delay(1000).queue(function() {
       jQuery(this).addClass("is-shown").dequeue();
     });
@@ -120,11 +161,13 @@ jQuery(function(){
 
   // resize
   jQuery(window).on("resize", function(){
-    index_top_resize();
+    indexTopResize();
 
-    index_top_scroll_down();
+    indexTopScrollDown();
 
-    index_article_resize();
+    indexBlogArticleResize();
+
+    indexArticleImgResize();
   });
 
 
