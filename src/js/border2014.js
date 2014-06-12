a1a6019d25413b977a61df8a92547eb19b10f6f9
RYO NAKAE <ryo.n.1129@gmail.com>
2014-06-13 02:46:26 +0900

    ブログまで完了+アニメーション少し実装

--- src/js/border2014.js
+++ src/js/border2014.js
@@ -76,6 +76,60 @@ jQuery(function(){
   */
 
 
+  // common logo show & hover
+  function commonLogoShowHover() {
+    jQuery(".logo").delay(1200).queue(function() {
+      jQuery(this).addClass("is-shown").dequeue();
+    });
+    jQuery(".logo").on({
+      "mouseenter":function(){
+        jQuery(this).removeClass("is-shown");
+        jQuery(this).delay(1).queue(function() {
+          jQuery(this).addClass("is-hover").dequeue();
+        });
+      },
+      "mouseleave":function(){
+        jQuery(this).removeClass("is-hover");
+        jQuery(this).addClass("is-shown");
+      }
+    });
+  }
+
+
+  // index change color
+  function indexChangeColor() {
+    var worksOffSet = jQuery(".l-index-works").offset().top;
+    var blogOffSet = jQuery(".l-index-blog").offset().top;
+    var footerOffSet = jQuery(".l-footer").offset().top;
+    var scroll;
+
+    jQuery(window).scroll(function(){
+      scroll = jQuery(window).scrollTop();
+
+      if ( scroll < 0 ) {
+        jQuery("body").removeClass("is-green");
+        jQuery("body").removeClass("is-blue");
+      }
+      else if ( 0 <= scroll && scroll <= worksOffSet + 100 ) {
+        jQuery("body").removeClass("is-green");
+        jQuery("body").removeClass("is-blue");
+      }
+      else if ( worksOffSet + 100 <= scroll && scroll <= blogOffSet + 100 ) {
+        jQuery("body").addClass("is-green");
+        jQuery("body").removeClass("is-blue");
+      }
+      else if ( blogOffSet + 100 <= scroll && scroll <= footerOffSet + 100 ) {
+        jQuery("body").removeClass("is-green");
+        jQuery("body").addClass("is-blue");
+      }
+      else {
+        jQuery("body").removeClass("is-green");
+        jQuery("body").removeClass("is-blue");
+      }
+    });
+  }
+
+
   // index top resize
   function indexTopResize() {
     jQuery(".l-index-top").css({
@@ -87,19 +141,17 @@ jQuery(function(){
 
   // index top scroll down
   function indexTopScrollDown() {
-    var worksOffset = jQuery(".l-index-works").offset();
-    var worksOffsetY = worksOffset.top;
-
+    // var worksOffset = jQuery(".l-index-works").offset().top;
     var topImage = jQuery(".index-top-image");
     var scroll;
     var topHeight;
 
     jQuery(".scroll.scroll-down").click(function(){
-      jQuery("html,body").animate({ scrollTop : worksOffsetY - 60 }, 1500, "easeInOutQuart");
+      jQuery("html,body").animate({ scrollTop : winHeight + 50 }, 1500, "easeInOutQuart");
     });
 
     jQuery(window).scroll(function(){
-      topHeight = jQuery(window).height();
+      topHeight = winHeight;
       scroll = jQuery(window).scrollTop();
 
       if ( scroll < 0 ) {
@@ -143,9 +195,9 @@ jQuery(function(){
   jQuery(window).on("load", function(){
     commonLoaderFadeOut();
 
-    jQuery(".logo").delay(1200).queue(function() {
-      jQuery(this).addClass("is-shown").dequeue();
-    });
+    commonLogoShowHover();
+
+    indexChangeColor();
 
     indexTopResize();
 
@@ -163,6 +215,8 @@ jQuery(function(){
 
   // resize
   jQuery(window).on("resize", function(){
+    indexChangeColor();
+
     indexTopResize();
 
     indexTopScrollDown();
