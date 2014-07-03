315e76acbd52b3b076e996904226aa00164b0e92
RYO NAKAE <ryo.n.1129@gmail.com>
2014-07-04 02:42:21 +0900

    GruntやめてMiddlemanに変更

--- config.rb
+++ config.rb
@@ -1,23 +1,74 @@
-# Define Directory
-http_path = "src"
-sass_dir = "src/sass"
-css_dir = "src/css"
-fonts_dir = "src/font"
-images_dir = "src/img"
-javascripts_dir = "src/js"
+###
+# Compass
+###
 
-# Define Output Style
-# Option: expanded(default) / nested / compact / compressed
-output_style = :compressed
+compass_config do |config|
+  config.output_style = :expanded
+  config.line_comments = false
+end
 
-# Define Comment
-line_comments = false
+###
+# Page options, layouts, aliases and proxies
+###
 
-# Define File Path
-relative_assets = true
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", layout: false
+#
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
 
-# Define SCSS or SASS
-# preferred_syntax = :scss
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
-# Disable .sass-cache
-cache = false
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Directory Settings
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :images_dir, 'img'
+set :fonts_dir, 'font'
+set :layouts_dir, 'layouts'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
